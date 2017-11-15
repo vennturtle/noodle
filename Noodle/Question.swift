d141a36b274564348aead0bae9c1f7e3a33297f9
Brandon Cecilio <mindlesshavock@gmail.com>
2017-11-14 21:58:35 -0800

    Added rudimentary question model, fixed some terminology for survey model

--- Noodle/Question.swift
+++ Noodle/Question.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+class Question {
+    var prompt: String
+    var type: QuestionType
+    var options: [String]
+    
+    enum QuestionType{
+        case TrueOrFalse
+        case SingleSelection
+        case MultipleSelection
+        
+        static func intToType(_ type: Int) -> QuestionType? {
+            switch (type){
+            case 0:
+                return .TrueOrFalse
+            case 1:
+                return .SingleSelection
+            case 2:
+                return .MultipleSelection
+            default:
+                return nil
+            }
+        }
+    }
+    
+    init(prompt: String, type: Int, options: [String]){
+        self.prompt = prompt
+        self.type = QuestionType.intToType(type)!
+        self.options = options
+    }
+    
+    convenience init(){
+        self.init(prompt: "", type: 0, options: [""])
+    }
+}
