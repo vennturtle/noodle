94b3543e3a754a01b5afc3b9c431197125d3d244
Brandon Cecilio <mindlesshavock@gmail.com>
2017-11-14 20:59:04 -0800

    implemented submitting surveys to Firebase in the model

--- Noodle/Survey.swift
+++ Noodle/Survey.swift
@@ -14,16 +14,15 @@ class Survey: NSObject {
     var id: String?
     var title: String
     var desc: String
-    var startDate: Date
-    var daysAvailable: Int
+    var startDate: Date?
+    var hoursAvailable: Int
     var latitude: Double
     var longitude: Double
     
-    init(title: String, desc: String, startDate: Date, daysAvailable: Int, latitude: Double, longitude: Double){
+    init(title: String, desc: String, hoursAvailable: Int, latitude: Double, longitude: Double){
         self.title = title
         self.desc = desc
-        self.startDate = startDate
-        self.daysAvailable = daysAvailable
+        self.hoursAvailable = hoursAvailable
         self.latitude = latitude
         self.longitude = longitude
     }
@@ -34,7 +33,7 @@ class Survey: NSObject {
         guard let title = dict["title"] as? String                  else { return nil }
         guard let desc = dict["desc"] as? String                    else { return nil }
         guard let startDateMillis = dict["startTime"] as? Int       else { return nil }
-        guard let daysAvailable = dict["hoursAvailable"] as? Int    else { return nil }
+        guard let hoursAvailable = dict["hoursAvailable"] as? Int   else { return nil }
         guard let latitude = dict["latitude"] as? Double            else { return nil }
         guard let longitude = dict["longitude"] as? Double          else { return nil }
         
@@ -42,8 +41,27 @@ class Survey: NSObject {
         self.title = title
         self.desc = desc
         self.startDate = Date(timeIntervalSince1970: Double(startDateMillis)/1000.0)
-        self.daysAvailable = daysAvailable
+        self.hoursAvailable = hoursAvailable
         self.latitude = latitude
         self.longitude = longitude
+    }
+    
+    convenience init(){
+        self.init(title: "", desc: "", hoursAvailable: 0, latitude: 0.0, longitude: 0.0)
+    }
+    
+    func submitNewSurvey(dbref: FIRDatabaseReference, authorID: String){
+        let currentDBTime = FIRServerValue.timestamp() as! [String:Any]
+        let key = dbref.child("Surveys").childByAutoId().key
+        let survey = ["uid": authorID,
+                      "title": self.title,
+                      "desc": self.description,
+                      "startTime": currentDBTime,
+                      "hoursAvailable": self.hoursAvailable,
+                      "latitude": self.latitude,
+                      "longitude": self.longitude] as [String: Any]
+        
+        let childUpdates = ["/Surveys/\(key)": survey]
+        dbref.updateChildValues(childUpdates)
     }
 }
