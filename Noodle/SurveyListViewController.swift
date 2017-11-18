74b6af102d14ee3e09ff718d49c78cbf5252546f
Brandon Cecilio <mindlesshavock@gmail.com>
2017-11-18 07:07:35 -0800

    edited surveylist to update if user's current location is > 10 meters away from the last updated location, fixed up syntax

--- Noodle/SurveyListViewController.swift
+++ Noodle/SurveyListViewController.swift
@@ -13,11 +13,6 @@ import CoreLocation
 import Firebase
 import FirebaseDatabase
 
-var currentSurveys: [Survey] = []
-var surveyUpdateHandle: FIRDatabaseHandle?
-var surveyUpdateQuery: FIRDatabaseQuery?
-
-
 class AnnotationDetails: NSObject, MKAnnotation{
     var identifier = "TakeSurvey"
     var title: String?
@@ -25,7 +20,6 @@ class AnnotationDetails: NSObject, MKAnnotation{
     var subtitle: String?
     var lat: Double?
     var long: Double?
-    
     
     init(title: String, subtitle: String, lat:CLLocationDegrees,long:CLLocationDegrees){
         self.title = title
@@ -35,15 +29,20 @@ class AnnotationDetails: NSObject, MKAnnotation{
         super.init()
     }
     
+    init(survey: Survey){
+        self.title = survey.title
+        self.subtitle = survey.desc
+        self.coordinate = CLLocationCoordinate2DMake(survey.latitude, survey.longitude)
+    }
 }
 
 class PinLocation: NSObject {
     //I think here is where I should load the surveys??
     var surveyPin = [AnnotationDetails]()
     override init(){
-        surveyPin += [AnnotationDetails(title:"THis is the Title",subtitle: "this is a long description to test what it would look like", lat:37.3352,long:-121.8811  )]
-        surveyPin += [AnnotationDetails(title:"SRVY2",subtitle: "desc",lat:37.337666,long: -121.885907 )]
-        surveyPin += [AnnotationDetails(title:"SRVY3",subtitle: "desc",lat:37.3382,long: -121.8812 )]
+        surveyPin += [AnnotationDetails(title:"THis is the Title",subtitle: "this is a long description to test what it would look like",lat:37.3352,long:-121.8811)]
+        surveyPin += [AnnotationDetails(title:"SRVY2",subtitle: "desc",lat:37.337666,long: -121.885907)]
+        surveyPin += [AnnotationDetails(title:"SRVY3",subtitle: "desc",lat:37.3382,long: -121.8812)]
     }
 }
 
@@ -51,9 +50,16 @@ class SurveyListViewController: UIViewController, MKMapViewDelegate,CLLocationMa
     @IBOutlet weak var mapView: MKMapView!
     
     let initialPin = CLLocationCoordinate2DMake(37.3300, -121.8800)
-    let surveys = PinLocation().surveyPin
+    //let surveys = PinLocation().surveyPin
     var myRef = FIRDatabase.database().reference()
-
+    
+    var currentSurveys: [Survey] = []
+    var surveyUpdateHandle: FIRDatabaseHandle?
+    var surveyUpdateQuery: FIRDatabaseQuery?
+    let regionRadius: CLLocationDistance = 1000 // 1KM ~ 0.621371 miles
+    let manager = CLLocationManager()
+    var selectedAnnotation: MKPointAnnotation!
+    var lastUpdatedLocation: CLLocation?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,51 +67,74 @@ class SurveyListViewController: UIViewController, MKMapViewDelegate,CLLocationMa
         // Do any additional setup after loading the view, typically from a nib.
         print("MY PRINTS WILL FOLLOW")
         manager.requestWhenInUseAuthorization()
-
-        Survey.getAll(near: (manager.location)!, radiusInKm: regionRadius, dbref: self.myRef) { surveys in
-            print("These are all within \(self.regionRadius) km of campus:")
-            for s in surveys {
-                print("\(s.id!): \(s.title) (\(s.latitude), \(s.longitude))")
-                print(currentSurveys)
-                var fuckit = [AnnotationDetails]()
-                fuckit += [AnnotationDetails(title: s.title,subtitle: s.desc,lat:s.latitude,long: s.longitude )]
-                self.mapView.addAnnotations(fuckit)
-
-            
-            }
-
-
-        }
-
-        
-        mapZoom()
-       // mapView.addAnnotations(surveys)
         mapView.delegate = self
-        
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.startUpdatingLocation()
+        mapZoom(at: initialPin, radius: 5000)
         
-                   }
-    private let regionRadius:CLLocationDistance = 1000 // 1KM ~ 0.621371 miles
+        // mapView.addAnnotations(surveys)
+    }
+    
+    func retrieveNearbyAndUpdate(){
+        self.lastUpdatedLocation = manager.location!
+        Survey.getAll(near: (lastUpdatedLocation)!, radiusInKm: regionRadius/1000, dbref: self.myRef) { surveys in
+            self.currentSurveys = surveys
+            var fuckit = [AnnotationDetails]()
+            let coord = self.lastUpdatedLocation!.coordinate
+            print("These are all within \(self.regionRadius/1000) km of \(coord):")
+            for s in surveys {
+                print("\(s.id!): \(s.title) (\(s.latitude), \(s.longitude))")
+                //print(currentSurveys)
+                fuckit += [AnnotationDetails(survey: s)]
+            }
+            
+            // update pins (only delete old pins out of range, and add new pins)
+            let oldPins = self.mapView.annotations
+            var toAdd = Set<AnnotationDetails>(fuckit)
+            let toDelete = oldPins.filter { old in
+                // only delete AnnotationDetails (i.e. not MKUserLocation)
+                if let oldAD = old as? AnnotationDetails {
+                    // only delete if it's not one of the new pins
+                    if toAdd.remove(oldAD) == nil { // returns nil if it's not one of the new pins
+                        return true
+                    }
+                }
+                return false
+            }
+            // delete out-of-range pins, add new pins,
+            // don't touch the rest of the old pins (or user's current location)
+            self.mapView.removeAnnotations(toDelete)
+            self.mapView.addAnnotations([AnnotationDetails](toAdd))
+            self.mapZoom(at: self.manager.location?.coordinate ?? self.initialPin)
+        }
+    }
+    
     func mapZoom(){
         let region = MKCoordinateRegionMakeWithDistance(initialPin, 5000, 5000)
         mapView.setRegion(region, animated: true)
     }
     
-    let manager = CLLocationManager()
+    func mapZoom(at: CLLocationCoordinate2D, radius: Double = 5000){
+        let region = MKCoordinateRegionMakeWithDistance(at, radius, radius)
+        mapView.setRegion(region, animated: true)
+    }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         self.mapView.showsUserLocation = true
-    }
-    //
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        if(status == CLAuthorizationStatus.denied){
-            showLocationisablePopUp()
+        if lastUpdatedLocation == nil || lastUpdatedLocation!.distance(from: manager.location!) > 10 {
+            retrieveNearbyAndUpdate()
         }
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if(status == CLAuthorizationStatus.denied){
+            showLocationIsAblePopUp()
+        }
+    }
+    
     //Create a Alert pop up that sends user to settings if they do not allow
-    func showLocationisablePopUp(){
+    func showLocationIsAblePopUp(){
         let alertController = UIAlertController(title: "Location Access Disabled", message: "In order for us to match you with surveys, we will need access to your location", preferredStyle: .alert)
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alertController.addAction(cancelAction)
@@ -113,38 +142,30 @@ class SurveyListViewController: UIViewController, MKMapViewDelegate,CLLocationMa
             if let url = URL(string: UIApplicationOpenSettingsURLString){
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
             }
-            
         }
         alertController.addAction(openAction)
         self.present(alertController, animated: true, completion: nil)
-        
     }
+    
     //make changes ot the annotation
-
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if let annotation = annotation as? AnnotationDetails{
             if let view = mapView.dequeueReusableAnnotationView(withIdentifier: annotation.identifier) as? MKPinAnnotationView {
                 return view
-            }else{
+            } else {
                 let view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotation.identifier)
                 view.isEnabled = true
                 view.canShowCallout = true
-//                view.detailCalloutAccessoryView = self.configureDetailView(annotationView: view)
-                view.leftCalloutAccessoryView = UIButton(type: .detailDisclosure)
-                
-                
-                
-                
-                
+                //view.detailCalloutAccessoryView = self.configureDetailView(annotationView: view)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
                 return view
             }
         }
         return nil
     }
     
-    var selectedAnnotation: MKPointAnnotation!
     func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-        if control == view.leftCalloutAccessoryView {
+        if control == view.rightCalloutAccessoryView {
             selectedAnnotation = view.annotation as? MKPointAnnotation
             performSegue(withIdentifier: "NextView", sender: self)
         }
@@ -154,24 +175,20 @@ class SurveyListViewController: UIViewController, MKMapViewDelegate,CLLocationMa
         if segue.destination is TestViewController {
         }
     }
-//    func configureDetailView(annotationView: MKAnnotationView) -> UIView {
-//        let snapshotView = UIView()
-//        let views = ["snapshotView": snapshotView]
-//        snapshotView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[snapshotView(200)]", options: [], metrics: nil, views: views))
-//        snapshotView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[snapshotView(75)]", options: [], metrics: nil, views: views))
-//        
-//        //do your work
-//        
-//        return snapshotView
-//    }
-
-
+    
+    /*func configureDetailView(annotationView: MKAnnotationView) -> UIView {
+        let snapshotView = UIView()
+        let views = ["snapshotView": snapshotView]
+        snapshotView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:[snapshotView(200)]", options: [], metrics: nil, views: views))
+        snapshotView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:[snapshotView(75)]", options: [], metrics: nil, views: views))
+        
+        //do your work
+        
+        return snapshotView
+    }*/
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-        
-        
     }
-
 }
 
